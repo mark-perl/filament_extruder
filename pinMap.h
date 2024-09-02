4237ad2075f8f1341d5ef6d086dd9ef6b3e2f0ce
Mark Perl <markperl1231@gmail.com>
2024-09-02 17:29:34 +1000

    Draft pin allocations

--- pinMap.h
+++ pinMap.h
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 // TODO: Check pinouts correct with uno unit
+// TODO: What pins are required for calipers
 
 // ###### Left Side ######
 // IOREF
@@ -11,30 +12,30 @@
 // GND
 // VIN
 
-const int UNUSED = A0;
-const int UNUSED = A1;
-const int UNUSED = A2;
+const int Pot_Dial_1 = A0;  // Tensioner Speed
+const int Pot_Dial_2 = A1;  // 
+const int Pot_Dial_3 = A2;
 const int UNUSED = A3;
-const int UNUSED = A4;
-const int UNUSED = A5;
+const int Display_Clock = A4;
+const int Display_Data = A5;
 
 // ###### Right Side ######
-const int UNUSED = SCL;
-const int UNUSED = SDA;
+const int UNUSED = SCL; //A5
+const int UNUSED = SDA; //A4
 // AREF
 // GND
-const int UNUSED = 13;
+const int Tranistor_1 = 13; // Fan power switch
 const int UNUSED = 12;
 const int UNUSED = 11;
 const int UNUSED = 10;
-const int UNUSED = 9;
-const int UNUSED = 8;
+const int Switch_1 = 9;     // (Feeder limit switch)
+const int Switch_2 = 8;     // Manual/Auto control toggle
 
-const int UNUSED = 7;
-const int UNUSED = 6;
-const int UNUSED = 5;
-const int UNUSED = 4;
-const int UNUSED = 3;
-const int UNUSED = 2;
-const int UNUSED = 1;
-const int UNUSED = 0;
+const int Z_Dir = 7;
+const int Y_Dir = 6;
+const int X_Dir = 5;
+const int Z_Step = 4;
+const int Y_Step = 3;
+const int X_Step = 2;
+const int Encoder_1 = 1;    // (Spool encoder)
+const int Encoder_2 = 0;    // (Spool encoder)

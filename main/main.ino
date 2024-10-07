d5002a648f2bfc8445fd90da22d49267101ccac0
mark-perl <markperl1231@gmail.com>
2024-10-07 11:12:23 +1100

    setup main structure, add parameters struct

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,55 @@
 #include "pinMap.h"
 #include "userInterface.h"
+#include "parameters.h"
 
 userInterface UI;
+parameters manualParams;
+parameters autoParams;
 
 void setup(){
+    Serial.begin(9600);
 
 }
 
 
 void loop(){
+
+    if (UI.selectPressed){
+        UI.selectPressed = 0;
+        // perform select actions
+    }
+
+    if (UI.enterPressed){
+        UI.enterPressed = 0;
+        // perform enter actions
+    }
+
+    UI.mode = UI.readMode();
+
+    switch (UI.mode)
+    {
+    case OFF:
+        
+        break;
+    
+    case AUTO:
+
+        break;
+
+    case MANUAL:
+
+        break;
+
+    default:
+        // do nothing
+        break;
+    }
+    
+
+
+
+
+
+
 
 }
---
+++ main/parameters.h
@@ -0,0 +1,8 @@
+
+
+struct parameters {
+    float spool_speed;  // steps/s
+    float feeder_speed;  // steps/s
+    float tensioner_speed;  // steps/s
+    int fans_on;  // num fans on / 5
+};
